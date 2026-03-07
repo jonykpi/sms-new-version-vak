e53fa7d7303130d7fbbc30639e234ec58016282b
jony <djony620@gmail.com>
2026-03-07 16:15:42 +0600

    add

--- server/cryptomus.js
+++ server/cryptomus.js
@@ -145,7 +145,7 @@ async function convertUsdToCryptoByRate(currency, usdAmount) {
 
   const url = `${API_BASE}/exchange-rate/${encodeURIComponent(cur)}/list`;
   const res = await fetch(url, { method: 'GET' });
-  if (!res.ok) throw new Error(`Exchange rate API error (${res.status})`);
+  if (!res.ok) throw new Error(`Exchange rate API error (${res.json().message})`);
 
   let data;
   try {
